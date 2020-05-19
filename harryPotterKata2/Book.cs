1a787d18589947928c2603094ef013c932bc4e4d
Sarah Weeks <sarah.weeks@footasylum.com>
2020-05-19 14:32:24 +0100

    Created Testing class and Book class in order to return price of single book

---
+++ harryPotterKata2/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace harryPotterKata2
+{
+    internal class Book
+    {
+        double singleBookPrice = 8.00;
+        public Book()
+        {
+
+        }
+
+        internal double GetPrice()
+        {
+            return singleBookPrice;
+        }
+    }
+}
---
+++ harryPotterKata2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace harryPotterKata2
+{
+    public class Class1
+    {
+        [Fact]
+        public void One_copy_of_any_book_costs_eight_pounds()
+        {
+            Book book = new Book();
+            
+            Assert.Equal(8.00, book.GetPrice());
+        }
+    }
+}
